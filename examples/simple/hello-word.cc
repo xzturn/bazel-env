2ce8ea11391eebc77e3cd681fa1528a908d2628b
xzturn <xzturn@outlook.com>
2015-11-19 17:04:25 +0800

    add hello world example using gtest

---
+++ examples/simple/hello-word.cc
@@ -0,0 +1,17 @@
+// Copyright (c) 2015, Xzturn Inc.
+// All Rights Reserved
+//
+// Hello world example
+//
+// Author:  superangevil (P.S.), peng.sunshine@gmail.com
+// Version: 1.0
+// Date:    11/19/2015 04:56:11 PM
+// File:    hello-word.cc
+//
+#include <stdio.h>
+
+#include "gtest/gtest.h"
+
+TEST(SimpleTest, HelloWorld) {
+  printf("Hello World!\n");
+}
